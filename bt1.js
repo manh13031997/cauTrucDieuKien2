3cd200e06c140075ed37d5622c0115b97f1f2460
manh13031997 <nguyenducmanh130397@gmail.com>
2023-07-28 09:24:36 +0700

    len

---
+++ bt1.js
@@ -0,0 +1,8 @@
+function chia() {
+    let x= +prompt(a,b);
+    if (x=0){
+        alert("a chia hết cho b")
+    }else{
+        alert("a không chia hết cho b")
+    }
+}
---
+++ tuoiHS.js
@@ -0,0 +1,12 @@
+function f() {
+    let tuoi= parseInt(document.getElementById("tuoi").value)
+    let cout;
+        if(tuoi>13){
+            alert("đủ tuổi")
+        }else{
+            alert("không đủ tuổi")
+        }
+        return 0;
+    }
+
+
